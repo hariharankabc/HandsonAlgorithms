ef21d3693ba3a3e73a0e08d419061b1967f1fa07
HAK <67498648+hariharankabc@users.noreply.github.com>
2021-06-06 22:43:02 +0530

    Update fraudulent-activity-notifications.java

--- src/org/algorithms/sort/fraudulent-activity-notifications.java
+++ src/org/algorithms/sort/fraudulent-activity-notifications.java
@@ -72,33 +72,4 @@ public class Solution {
     }
 
     private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] nd = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(nd[0]);
-
-        int d = Integer.parseInt(nd[1]);
-
-        int[] expenditure = new int[n];
-
-        String[] expenditureItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int expenditureItem = Integer.parseInt(expenditureItems[i]);
-            expenditure[i] = expenditureItem;
-        }
-
-        int result = activityNotifications(expenditure, d);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
-    }
 }
